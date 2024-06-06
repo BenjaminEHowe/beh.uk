eb90dd50a47b26a2f4d3bb3755a5c40a0e3e1f1c
Benjamin Howe <BenjaminEHowe@users.noreply.github.com>
2024-06-06 18:37:43 +0100

    Generify form handling (#38)
    * Replace `bodyToText` with `formToText`
    
    * `const` --> `var`
    
    * Call `JSON.stringify` where required
    
    * Replace `JSON.stringify` with `prettyJson`
    
    * Inline `formData`
    
    * Re-order params for `sendFormViaResend`
    
    * Remove stray space
    
    * Extract methods to reusable import
    
    * Updated honeypot handling
    
    * Added email Reply-To header

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -1,56 +1,30 @@
+import { sendFormViaEmail } from "../../functions-src/comms/email.js"
+
 export async function onRequest(context) {
   if (context.request.method !== "POST") {
     return new Response("Invalid request method", { status: 405 });
   }
 
   const url = new URL(context.request.url)
-  const formData = await context.request.formData();
-  const body = Object.fromEntries(formData.entries());
-  body.cf = context.request.cf;
-  body.headers = Object.fromEntries(context.request.headers.entries());
+  const form = Object.fromEntries((await context.request.formData()).entries());
+  form.cf = context.request.cf;
+  form.headers = Object.fromEntries(context.request.headers.entries());
 
-  const sent = await sendFormViaResend(body, context.env.EMAIL_BEN, context.env.RESEND_KEY);
+  if (form.name === "") {
+    form.name = "blank, good, honeypot passed"
+  } else {
+    form.name += " (BAD, honeypot failed)"
+  }
+
+  const sent = await sendFormViaEmail({
+    env: context.env,
+    subject: "New contact form submission from beh.uk",
+    form,
+  });
 
   if (!sent) {
     return new Response("Oops! Something went wrong. Please try submitting the form again.", { status: 500 });
   }
 
   return Response.redirect(`https://${url.hostname}/contact-success`, 303);
-}
-
-
-function bodyToText(body) {
-  return `Email from ${body.email}
-
-"name" (honeypot, should be blank): ${body.name}
-
-Message:
-${body.message}
-
-headers:
-${prettyJson(body.headers)}
-
-cf:
-${prettyJson(body.cf)}`
-}
-
-async function sendFormViaResend(body, email, api_key) {
-  const response = await fetch(new Request("https://api.resend.com/emails", {
-    method: "POST",
-    headers: {
-      "Authorization": `Bearer ${api_key}`,
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      from: "no-reply@viaresend.beh.uk",
-      to: email,
-      subject: "New contact form submission from beh.uk",
-      text: bodyToText(body),
-    }),
-  }));
-  return response.ok
-}
-
-function prettyJson(json) {
-  return JSON.stringify(json, null, 2);
 }
