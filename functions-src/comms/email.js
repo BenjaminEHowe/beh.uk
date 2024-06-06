9a347dc74948c6634b996af2cc5df97d96c5b96a
Benjamin Howe <ben@beh.uk>
2024-06-06 18:29:47 +0100

    Added email Reply-To header

--- functions-src/comms/email.js
+++ functions-src/comms/email.js
@@ -4,21 +4,25 @@ function formToText(form) {
     text += `${property}:\n`;
     var value = form[property];
     if (typeof value !== "string") {
-      value = prettyJson(value)
+      value = prettyJson(value);
     }
     text += `${value}\n\n`;
   }
-  return text
+  return text;
 }
 
 async function sendFormViaEmail({env, to, subject, form}) {
   if (to === undefined) {
-    to = env.EMAIL_BEN
+    to = env.EMAIL_BEN;
   }
-  return await sendFormViaResend(env.RESEND_KEY, to, subject, form)
+  return await sendFormViaResend(env.RESEND_KEY, to, subject, form);
 }
 
 async function sendFormViaResend(api_key, to, subject, form) {
+  const headers = {}
+  if (FORM_EMAIL_KEY in form) {
+    headers["Reply-To"] = form[FORM_EMAIL_KEY]
+  }
   const response = await fetch(new Request("https://api.resend.com/emails", {
     method: "POST",
     headers: {
@@ -30,13 +34,16 @@ async function sendFormViaResend(api_key, to, subject, form) {
       to,
       subject,
       text: formToText(form),
+      headers,
     }),
   }));
-  return response.ok
+  return response.ok;
 }
 
 function prettyJson(json) {
   return JSON.stringify(json, null, 2);
 }
+
+const FORM_EMAIL_KEY = "email";
 
 export { sendFormViaEmail }
