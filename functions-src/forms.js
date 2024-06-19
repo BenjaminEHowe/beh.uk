b01605141b1bafba940cea8062269354d2eec8fa
Benjamin Howe <ben@beh.uk>
2024-06-19 22:17:09 +0100

    Added basic D1 code

---
+++ functions-src/forms.js
@@ -0,0 +1,41 @@
+import { sendFormViaEmail } from "comms/email.js"
+import { ulidFactory } from "ulid.ts"
+
+const ulid = ulidFactory({ monotonic: true });
+
+async function handleForm({
+  context,
+  formId,
+  honeypotField,
+}) {
+  const submissionId = ulid();
+  const submittedTs = new Date().toISOString();
+  const replyEmail = form["email"];
+  const fields = Object.fromEntries((await context.request.formData()).entries());
+  const spamReasons = [];
+  const cf = context.request.cf;
+  const headers = Object.fromEntries(context.request.headers.entries());
+
+  if (typeof honeypotField === "String") {
+    if (fields[honeypotField] !== "") {
+      spamReasons.push("HONEYPOT");
+    }
+  }
+
+  await context.env.DB_FORMS.prepare("INSERT INTO submissions (submission_id, form_id, submitted_ts, reply_email, fields, spam_reasons, cf, headers) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
+    .bind(submissionId, formId, submittedTs, replyEmail, fields, spamReasons, cf, headers)
+    .run();
+
+  if (spamReasons.length === 0) {
+    return await sendFormViaEmail({
+      env: context.env,
+      subject: `New ${formId} form submission from beh.uk`,
+      form,
+    })
+  } else {
+    // submission is belived to be spam, return OK and don't do anything else
+    return true;
+  }
+}
+
+export { handleForm }
---
+++ sql/beh-uk-forms-schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Customers (
+  submission_id TEXT PRIMARY KEY,
+  form_id TEXT NOT NULL,
+  submitted_ts DATETIME NOT NULL,
+  reply_email TEXT,
+  fields TEXT NOT NULL,
+  cf TEXT NOT NULL,
+  headers TEXT NOT NULL,
+);
